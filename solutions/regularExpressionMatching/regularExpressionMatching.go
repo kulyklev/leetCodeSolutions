aeb12b23cb87e715960f99a76893c47bc71931a5
Lev Kulyk <kulyklev.dev@gmail.com>
2022-07-09 18:39:20 +0300

    implement single char placeholder

--- solutions/regularExpressionMatching/regularExpressionMatching.go
+++ solutions/regularExpressionMatching/regularExpressionMatching.go
@@ -1,5 +1,32 @@
 package regularExpressionMatching
 
 func isMatch(s string, p string) bool {
-	return s == p
+	const oneChar = '.'
+
+	ji := 0
+	for i := 0; i < len(p); i++ {
+		patternRune := p[i]
+
+		if ji == len(s) {
+			return false
+		}
+
+	strLoop:
+		for j := ji; j < len(s); j++ {
+			strRune := s[j]
+
+			switch true {
+			case patternRune == oneChar:
+				ji = j + 1
+				break strLoop
+			case patternRune == strRune:
+				ji = j + 1
+				break strLoop
+			default:
+				return false
+			}
+		}
+	}
+
+	return true
 }
--- solutions/regularExpressionMatching/regularExpressionMatching_test.go
+++ solutions/regularExpressionMatching/regularExpressionMatching_test.go
@@ -2,31 +2,130 @@ package regularExpressionMatching
 
 import "testing"
 
+// Success and failure markers.
+const (
+	success = "\u2713"
+	failed  = "\u2717"
+)
+
 func TestIsMatch(t *testing.T) {
-	for scenario, fn := range map[string]func(t *testing.T){
-		"no pattern match":    testNoPatternMatch,
-		"no pattern mismatch": testNoPatternMismatch,
-	} {
-		t.Run(scenario, func(t *testing.T) {
-			fn(t)
-		})
+
+	t.Log("Given the need to check regexp match.")
+	{
+		for scenario, fn := range map[string]func(t *testing.T){
+			"no pattern match":                 testNoPatternMatch,
+			"no pattern mismatch":              testNoPatternMismatch,
+			"match one single character":       testMatchSingleCharacter,
+			"match multiple single characters": testMatchMultipleSingleCharacters,
+		} {
+			t.Run(scenario, func(t *testing.T) {
+				fn(t)
+			})
+		}
 	}
 }
 
 func testNoPatternMatch(t *testing.T) {
-	got := isMatch("qwe", "w")
-	want := true
 
-	if got != want {
-		t.Errorf("got %t, want %t", got, want)
+	testID := 0
+	t.Logf("\t\tTest %d:\tWhen matching without pattern.", testID)
+	{
+		inputString := "qwe"
+		pattern := "qwe"
+		got := isMatch(inputString, pattern)
+		want := true
+
+		if got != want {
+			t.Fatalf("\t%s\tTest %d:\tExpected: %t. String: %s. Does not match: %s", failed, testID, want, inputString, pattern)
+		}
+
+		t.Logf("\t%s\tTest %d:\tString matches pattern.", success, testID)
 	}
 }
 
 func testNoPatternMismatch(t *testing.T) {
-	got := isMatch("qwe", "w")
-	want := false
 
-	if got != want {
-		t.Errorf("got %t, want %t", got, want)
+	testID := 0
+	t.Logf("\t\tTest %d:\tWhen not matching without pattern.", testID)
+	{
+		inputString := "qwe"
+		pattern := "qwerty"
+		got := isMatch(inputString, pattern)
+		want := false
+
+		if got != want {
+			t.Fatalf("\t%s\tTest %d:\tExpected: %t. String: %s. Match pattern: %s", failed, testID, want, inputString, pattern)
+		}
+
+		t.Logf("\t%s\tTest %d:\tString mismatches.", success, testID)
+	}
+}
+
+func testMatchSingleCharacter(t *testing.T) {
+	testID := 0
+	t.Logf("\t\tTest %d:\tWhen matching pattern with singe character.", testID)
+	{
+		inputString := "a"
+		pattern := "."
+		got := isMatch(inputString, pattern)
+		want := true
+
+		if got != want {
+			t.Fatalf("\t%s\tTest %d:\tExpected: %t. String: \"%s\" should match pattern: \"%s\".", failed, testID, want, inputString, pattern)
+		}
+
+		t.Logf("\t%s\tTest %d:\tPattern matches.", success, testID)
+	}
+}
+
+func testMatchMultipleSingleCharacters(t *testing.T) {
+
+	type testCases struct {
+		description  string
+		inputString  string
+		inputPattern string
+		expected     bool
+	}
+
+	testID := 0
+	t.Logf("\t\tTest %d:\tWhen matching pattern with multiple signle characters.", testID)
+	{
+		for _, scenario := range []testCases{
+			{
+				description:  "Two duplicate chars",
+				inputString:  "aa",
+				inputPattern: "..",
+				expected:     true,
+			},
+			{
+				description:  "Two different chars",
+				inputString:  "ab",
+				inputPattern: "..",
+				expected:     true,
+			},
+			{
+				description:  "Placeholders at edges",
+				inputString:  "cabd",
+				inputPattern: ".ab.",
+				expected:     true,
+			},
+		} {
+			t.Run(scenario.description, func(t *testing.T) {
+				actual := isMatch(scenario.inputString, scenario.inputPattern)
+
+				if actual != scenario.expected {
+
+					if scenario.expected {
+						t.Fatalf("\t%s\tTest %d:\tExpected: %t. String: \"%s\" should match pattern: \"%s\".", failed, testID, scenario.expected, scenario.inputString, scenario.inputPattern)
+					} else {
+						t.Fatalf("\t%s\tTest %d:\tExpected: %t. String: \"%s\" should NOT match pattern: \"%s\".", failed, testID, scenario.expected, scenario.inputString, scenario.inputPattern)
+					}
+				}
+
+				t.Logf("\t%s\tTest %d:\tPattern matches.", success, testID)
+				/*require.Error(t, err)
+				assert.Equal(t, scenario.expectedError, err.Error())*/
+			})
+		}
 	}
 }
