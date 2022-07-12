42b4469b0f37e7e4b48bc320289d54cdae363e13
Lev Kulyk <kulyklev.dev@gmail.com>
2022-07-12 11:03:52 +0300

    implement regex matching

--- solutions/regularExpressionMatching/regularExpressionMatching.go
+++ solutions/regularExpressionMatching/regularExpressionMatching.go
@@ -4,6 +4,31 @@ func isMatch(s string, p string) bool {
 	const zeroOrMore = '*' // 42
 	const oneChar = '.'    // 46
 
+	dp := make([][]bool, len(s)+1)
+	for i, _ := range dp {
+		dp[i] = make([]bool, len(p)+1)
+	}
+	dp[len(s)][len(p)] = true
+
+	for i := len(s); i >= 0; i-- {
+		for j := len(p) - 1; j >= 0; j-- {
+			firstMatch := i < len(s) && (p[j] == s[i] || p[j] == oneChar)
+
+			if j+1 < len(p) && p[j+1] == zeroOrMore {
+				dp[i][j] = dp[i][j+2] || firstMatch && dp[i+1][j]
+			} else {
+				dp[i][j] = firstMatch && dp[i+1][j+1]
+			}
+		}
+	}
+
+	return dp[0][0]
+}
+
+func isMatchShittyImplementation(s string, p string) bool {
+	const zeroOrMore = '*' // 42
+	const oneChar = '.'    // 46
+
 	revS := reverse(s)
 	revP := reverse(p)
 
